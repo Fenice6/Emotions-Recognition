df4b19f1154a24764733d31d4f09f73ae92104ac
Francesco Lapiana <fenice-6@libero.it>
2020-03-05 11:14:41 +0100

    [Update] AmazonIdentification: faceAnalyzerAsync
    Added function to analyze feelings of a specific face.

--- Recognition/AmazonProvider/AmazonIdentification.cs
+++ Recognition/AmazonProvider/AmazonIdentification.cs
@@ -1,13 +1,20 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Threading.Tasks;
 using Amazon.Rekognition;
 using Amazon.Rekognition.Model;
+using Microsoft.AspNetCore.Http;
+using System.Text.Json;
 
 namespace AmazonProvider
 {
     public class AmazonIdentification
     {
         private readonly IAmazonRekognition _amazonRekognition;
+        private readonly List<string> detectionAttributes = new List<string> { "ALL" };
+        private readonly QualityFilter qualityFilter = QualityFilter.HIGH;
+        private readonly int maxFaces = 1;
         public AmazonIdentification(IAmazonRekognition amazonRekognition)
         {
             _amazonRekognition = amazonRekognition;
@@ -23,6 +30,34 @@ namespace AmazonProvider
             );
             if (createCollectionResponse.HttpStatusCode != System.Net.HttpStatusCode.OK)
                 throw new Exception(createCollectionResponse.HttpStatusCode.ToString());
+        }
+
+        public async Task<string> faceAnalyzerAsync(IFormFile photoFile, string collectionID)
+        {
+            MemoryStream memoryStream = new MemoryStream();
+            photoFile.CopyTo(memoryStream);
+
+            IndexFacesRequest indexFacesRequest = new IndexFacesRequest
+            {
+                CollectionId = collectionID,
+                DetectionAttributes = detectionAttributes,
+                MaxFaces = maxFaces,
+                QualityFilter = qualityFilter,
+                Image = new Image
+                {
+                    Bytes = memoryStream,
+                }
+            };
+
+            IndexFacesResponse indexFacesResponse = await this._amazonRekognition.IndexFacesAsync(indexFacesRequest);
+
+            if (indexFacesResponse.HttpStatusCode != System.Net.HttpStatusCode.OK)
+                throw new Exception(indexFacesResponse.HttpStatusCode.ToString());
+
+            if (indexFacesResponse.UnindexedFaces.Count > 0 || indexFacesResponse.FaceRecords.Count != 1)
+                throw new Exception("Unindexed Faces: " + indexFacesResponse.UnindexedFaces.Count + "\nIndexed Faces: " + indexFacesResponse.FaceRecords.Count);
+
+            return JsonSerializer.Serialize(indexFacesResponse.FaceRecords[0]);
         }
     }
 }
