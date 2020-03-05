56c6f3870a4e2fcbdb9dc7709e1a995ae51ea632
Francesco Lapiana <fenice-6@libero.it>
2020-03-05 11:45:57 +0100

    [Update] GenericCloudWrapperProvider: load implementation by config
    Load correct implementation of Identification by configuration with reflection.

--- Recognition/GenericCloudProvider/GenericCloudWrapperProvider.cs
+++ Recognition/GenericCloudProvider/GenericCloudWrapperProvider.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Reflection;
 using GenericCloudCommons.Interfaces;
 using Microsoft.Extensions.Configuration;
 
@@ -15,7 +17,21 @@ namespace GenericCloudProvider
 
         public GenericCloudWrapperProvider(IConfiguration config)
         {
-            
+            var dllName = config.GetSection("GenericCloudProvider:DllName");
+            if (dllName.Value is null)
+                throw new MissingFieldException("GenericCloudProvider: dll name miss");
+            var parentFolder = Path.GetDirectoryName(System.Reflection.Assembly.GetEntryAssembly().Location);
+            var absolutePath = Path.Combine(parentFolder, dllName.Value);
+            var dllFile = new FileInfo(absolutePath);
+            if (!dllFile.Exists)
+                throw new FileNotFoundException("GenericCloudProvider dll miss");
+            var provider = Assembly.LoadFile(dllFile.FullName);
+
+            var identificationClass = config.GetSection("GenericCloudProvider:IdentificationClass");
+            if (identificationClass.Value is null)
+                throw new MissingFieldException("GenericCloudProvider:IdentificationClass miss");
+            var identificationType = provider.GetType(identificationClass.Value);
+            _identification = (IIdentification)Activator.CreateInstance(identificationType, config);
         }
     }
 }
