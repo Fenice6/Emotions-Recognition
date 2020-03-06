e6bfa6e3d36992cfeb65fddbcb60f3212acbfeb7
Francesco Lapiana <fenice-6@libero.it>
2020-03-06 17:22:06 +0100

    [Rename/Update] FaceEmotionsController
    FaceFeelingsController is renamed in FaceEmotionsController.
    FaceEmotionsController is using EmotionExtractor service so reflection is used only at first call and not for each call.
    GetEmotionsReport post is used to generate report.

---
+++ Recognition/Recognition/Controllers/FaceEmotionsController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading.Tasks;
+using GenericCloudProvider;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Recognition.Services.Interfaces;
+using RecognitionCommons.DataModels;
+
+namespace Recognition.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FaceEmotionsController : ControllerBase
+    {
+        private readonly IEmotionsExtractor _emotionsExtractor;
+        public FaceEmotionsController(IEmotionsExtractor emotionsExtractor)
+        {
+            _emotionsExtractor = emotionsExtractor;
+        }
+
+        [HttpPost]
+        public async Task<EmotionsReport> GetEmotionsReport([FromForm] EmotionsRecognitionInputDatas emotionsRecognitionInputDatas)
+        {
+            return new EmotionsReport()
+            {
+                Report = await this._emotionsExtractor.EmotinsReport(
+                    emotionsRecognitionInputDatas.PhotoInput
+                )
+            };
+        }
+    }
+}
