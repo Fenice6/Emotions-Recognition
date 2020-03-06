023ac51ea5df069ab9031bb1a159ce5ed9df86ae
Francesco Lapiana <fenice-6@libero.it>
2020-03-06 17:17:32 +0100

    [Add] Service: EmotionExtractor
    This service create an emotions report using cloud services.

---
+++ Recognition/Recognition/Services/EmotionsExtractor.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading.Tasks;
+using GenericCloudProvider;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Recognition.Services.Interfaces;
+
+namespace Recognition.Services
+{
+    public class EmotionsExtractor : IEmotionsExtractor
+    {
+        private readonly GenericCloudWrapperProvider _genericCloudWrapperProvider;
+        private readonly IConfiguration _config;
+        public EmotionsExtractor(IConfiguration config)
+        {
+            _genericCloudWrapperProvider = new GenericCloudWrapperProvider(config);
+            _config = config;
+        }
+        public async Task<string> EmotinsReport(IFormFile PhotoInput)
+        {
+            string collectionID = Guid.NewGuid().ToString();
+            await this._genericCloudWrapperProvider.Identification.GenerateNewCollectionAsync(collectionID);
+            return await this._genericCloudWrapperProvider.Identification.FaceAnalyzerAsync(
+                    PhotoInput,
+                    collectionID
+                );
+        }
+    }
+}
