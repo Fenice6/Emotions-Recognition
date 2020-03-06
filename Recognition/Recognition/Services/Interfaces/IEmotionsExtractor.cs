2cf366af5e976b690a3e86bc4194638e37319a23
Francesco Lapiana <fenice-6@libero.it>
2020-03-06 17:16:08 +0100

    [Add] Service interface: IEmotionsExtractor
    This interface is used by emotions extractors. It must create an emotions report.

---
+++ Recognition/Recognition/Services/Interfaces/IEmotionsExtractor.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace Recognition.Services.Interfaces
+{
+    public interface IEmotionsExtractor
+    {
+        Task<string> EmotinsReport(IFormFile PhotoInput);
+    }
+}
